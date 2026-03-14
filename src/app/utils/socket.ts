657c9d224199595d5c4cb1d222fe38e6ae36c2ea
imamhossenbu <imam.cse9.bu@gmail.com>
2026-03-14 15:03:17 +0600

    modify

--- src/app/utils/socket.ts
+++ src/app/utils/socket.ts
@@ -3,6 +3,19 @@ import { Server as HttpServer } from 'http';
 import { ChatService } from '../../modules/chat/chat.service';
 import { TrackingService } from '../../modules/tracking/tracking.service';
 
+interface IChatData {
+  orderId: string;
+  sender: string;
+  senderModel: 'User' | 'Rider';
+  message: string;
+}
+
+interface ILocationData {
+  orderId: string;
+  currentLocation: { lat: number; lng: number };
+  status: string;
+}
+
 export const setupSocket = (server: HttpServer) => {
   const io = new SocketServer(server, {
     cors: {
@@ -12,18 +25,18 @@ export const setupSocket = (server: HttpServer) => {
   });
 
   io.on('connection', (socket) => {
-    console.log('⚡ A user connected:', socket.id);
+    console.log(' A user connected:', socket.id);
 
 
     socket.on('join-order', (orderId: string) => {
       socket.join(orderId);
-      console.log(`👤 User joined order room: ${orderId}`);
+      console.log(` User joined order room: ${orderId}`);
     });
 
 
-    socket.on('send-message', async (data) => {
+    socket.on('send-message', async (data:IChatData) => {
       try {
-        const savedMsg = await ChatService.saveMessage(data);
+        const savedMsg = await ChatService.saveMessage(data as any);
         
         io.to(data.orderId).emit('new-message', savedMsg);
       } catch (error) {
@@ -31,10 +44,9 @@ export const setupSocket = (server: HttpServer) => {
       }
     });
 
-    socket.on('update-location', async (data) => {
+    socket.on('update-location', async (data:ILocationData) => {
       try {
-        await TrackingService.updateLiveLocation(data);
-        
+        await TrackingService.updateLiveLocation(data as any);
         io.to(data.orderId).emit('location-updates', data.currentLocation);
       } catch (error) {
         console.error('Socket location error:', error);
