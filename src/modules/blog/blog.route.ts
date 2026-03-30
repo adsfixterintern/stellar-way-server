b2e7fa150ef717a0606dd535c0aa3e2e16f4db13
imamhossenbu <imam.cse9.bu@gmail.com>
2026-03-30 16:36:24 +0600

    modify blog route

--- src/modules/blog/blog.route.ts
+++ src/modules/blog/blog.route.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { BlogController } from './blog.controller';
 import { upload } from '../../app/config/cloudinary.config';
+import { isAuthenticated } from '../../app/middlewares/auth.middleware';
+import { authorizeRoles } from '../../app/middlewares/authorization.middleware';
 
 const router = express.Router();
 
@@ -9,14 +11,14 @@ router.post(
   upload.fields([
     { name: 'thumbnail', maxCount: 1 },
     { name: 'sectionImages', maxCount: 10 }
-  ]),
+  ]),isAuthenticated,authorizeRoles('admin'),
   BlogController.createBlog
 );
 
-router.get('/', BlogController.getAllBlogs);
+router.get('/',isAuthenticated, BlogController.getAllBlogs);
 
 
-router.get('/:id', BlogController.getSingleBlog);
+router.get('/:id',isAuthenticated, BlogController.getSingleBlog);
 
 
 router.patch(
@@ -24,11 +26,11 @@ router.patch(
   upload.fields([
     { name: 'thumbnail', maxCount: 1 },
     { name: 'sectionImages', maxCount: 10 }
-  ]),
+  ]),isAuthenticated,authorizeRoles('admin'),
   BlogController.updateBlog
 );
 
 
-router.delete('/:id', BlogController.deleteBlog);
+router.delete('/:id',isAuthenticated,authorizeRoles('admin'), BlogController.deleteBlog);
 
 export const BlogRoutes = router;
