39a9b9bc7058fd5f992442170a3288394c8e0fc8
imamhossenbu <imam.cse9.bu@gmail.com>
2026-04-28 12:03:39 +0600

    modify event

--- src/modules/event/event.route.ts
+++ src/modules/event/event.route.ts
@@ -1,20 +1,31 @@
-import { Router } from 'express';
-import { EventControllers } from './event.controller';
-import { upload } from '../../app/config/cloudinary.config';
-import { isAuthenticated } from '../../app/middlewares/auth.middleware';
-import { authorizeRoles } from '../../app/middlewares/authorization.middleware';
+import { Router } from "express";
+import { EventControllers } from "./event.controller";
+import { upload } from "../../app/config/cloudinary.config";
+import { isAuthenticated } from "../../app/middlewares/auth.middleware";
+import { authorizeRoles } from "../../app/middlewares/authorization.middleware";
 
 const router = Router();
 
 router.post(
-  '/create-event',
-  upload.single('image'),
+  "/create-event",
+  upload.single("image"),
   isAuthenticated,
-  authorizeRoles('admin'),
-  EventControllers.createEvent
+  authorizeRoles("admin"),
+  EventControllers.createEvent,
 );
-router.get("/event",EventControllers.getAllEvents);
-router.get("/event/:id",isAuthenticated,authorizeRoles('admin','chef','rider','user'), EventControllers.getSingleEvent);
-router.put("/event/:id", upload.single("image"),isAuthenticated,authorizeRoles('admin'),EventControllers.updateEvent);
-router.delete("/event/:id",isAuthenticated,authorizeRoles('admin'), EventControllers.deleteEvent);
+router.get("/event", EventControllers.getAllEvents);
+router.get("/event/:id", EventControllers.getSingleEvent);
+router.put(
+  "/event/:id",
+  upload.single("image"),
+  isAuthenticated,
+  authorizeRoles("admin"),
+  EventControllers.updateEvent,
+);
+router.delete(
+  "/event/:id",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  EventControllers.deleteEvent,
+);
 export const EventRoutes = router;
